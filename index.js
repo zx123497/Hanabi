fd78b620d9ff12921f7ac99aed491a65f1837fe3
leo <leo000111444@gmail.com>
2021-08-09 14:58:43 +0800

    init

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+const socket = require("socket.io");
+const color = require("colors");
+const cors = require("cors");
+const { get_Current_User, user_Disconnect, join_User } = require("./dummyuser");
+
+app.use(express());
+
+const port = 8000;
+
+app.use(cors());
+
+var server = app.listen(
+  port,
+  console.log(`Server is running on the port no: ${port} `.green)
+);
+
+const io = socket(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+//initializing the socket io connection
+io.on("connection", (socket) => {
+  //for a new user joining the room
+  socket.on("joinRoom", ({ username, roomname }) => {
+    //* create user
+    const p_user = join_User(socket.id, username, roomname);
+    console.log(socket.id, "=id");
+    socket.join(p_user.room);
+
+    //display a welcome message to the user who have joined a room
+    socket.emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: `歡迎加入花火大會， ${p_user.username}`,
+    });
+
+    //displays a joined room message to all other room users except that particular user
+    socket.broadcast.to(p_user.room).emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: `${p_user.username} 也加入花火大會了，打聲招呼吧~`,
+    });
+  });
+
+  //user sending message
+  socket.on("chat", (text) => {
+    //gets the room user and the message sent
+    const p_user = get_Current_User(socket.id);
+
+    io.to(p_user.room).emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: text,
+    });
+  });
+
+  socket.on("firework", (data) => {
+    //gets the room user and the message sent
+    const p_user = get_Current_User(socket.id);
+
+    io.to(p_user.room).emit("hanabi", {
+      userId: p_user.id,
+      cordin: data.cordin,
+      color: data.colors,
+    });
+  });
+
+  //when the user exits the room
+  socket.on("disconnect", () => {
+    //the user is deleted from array of users and a left room message displayed
+    const p_user = user_Disconnect(socket.id);
+
+    if (p_user) {
+      io.to(p_user.room).emit("message", {
+        userId: p_user.id,
+        username: p_user.username,
+        text: `${p_user.username} 離開花火大會 ~`,
+      });
+    }
+  });
+});
